b6475a6ea5c4ef169b3ea852c5e53d0c184f1a37
git <1120309488@qq.com>
2019-01-05 16:26:31 +0800

    change

---
+++ permiss/controller/Department.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rxw
+ * Date: 2017/8/27
+ * Time: 20:58
+ */
+namespace permiss\controller;
+use permiss\lib\User;
+
+class Department extends User
+{
+    protected function afterInit()
+    {
+    }
+
+    public function addApi()
+    {
+
+    }
+
+    public function getApi()
+    {
+
+    }
+
+    public function deleteApi()
+    {
+
+    }
+
+    public function updateApi()
+    {
+      
+    }
+}
---
+++ permiss/controller/Job.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: rxw
+ * Date: 2017/8/27
+ * Time: 20:58
+ */
+namespace permiss\controller;
+use permiss\lib\User;
+
+class Job extends User
+{
+    protected function afterInit()
+    {
+    }
+
+    public function addApi()
+    {
+
+    }
+
+    public function listApi()
+    {
+
+    }
+
+    public function deleteApi()
+    {
+
+    }
+
+    public function updateApi()
+    {
+      
+    }
+}
