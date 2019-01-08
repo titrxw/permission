615d639d2b7187e1f962920d0f739915ad3698fd
git <1120309488@qq.com>
2019-01-08 17:38:04 +0800

    change

--- vue/vue.config.js
+++ vue/vue.config.js
@@ -13,5 +13,16 @@ module.exports = {
    },
   lintOnSave: false,
   runtimeCompiler: true,
-  baseUrl: './'
+  baseUrl: './',
+  // https://segmentfault.com/a/1190000010613010
+  // http://www.jintiankansha.me/t/Gyy9pDe3Ei
+  // configureWebpack: {
+  //   plugins: [
+  //     new PrerenderSpaPlugin(
+  //       path.join(__dirname, 'dist'),
+  //        需要预渲染的路由   路由模式需要切换成history
+  //       ['/', '/login', '/platform/company', '/platform/comment']
+  //     )
+  //   ]
+  // }
 }
