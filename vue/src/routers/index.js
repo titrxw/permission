664ad4ed83d38e95ebd19a19aa8927677374fdb2
titrxw <1120309488@qq.com>
2019-01-09 22:41:29 +0800

    change

--- vue/src/routers/index.js
+++ vue/src/routers/index.js
@@ -6,25 +6,25 @@ import iView from 'iview';
 Vue.use(VueRouter);
 
 const router = new VueRouter({
-  routes: Routers
+    routes: Routers
 });
 
 router.beforeEach((to, from, next) => {
-  let title = '生活服务'
-  if (to.meta.title && to.meta.title !== '') {
-    title = to.meta.title
-  }
-  document.title = title
-  iView.LoadingBar.start();
+    let title = '生活服务'
+    if (to.meta.title && to.meta.title !== '') {
+        title = to.meta.title
+    }
+    document.title = title
+    iView.LoadingBar.start();
 
-  if (login(to, from, next)) {
-    next()
-  }
+    if (login.validate(to, from, next)) {
+        next()
+    }
 });
 
 router.afterEach((to, from, next) => {
-  iView.LoadingBar.finish();
-  window.scrollTo(0, 0);
+    iView.LoadingBar.finish();
+    window.scrollTo(0, 0);
 });
 
 export default router;
