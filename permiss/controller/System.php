99b2fc286a22ed031f4db091b4ef382296bc429d
titrxw <1120309488@qq.com>
2019-02-13 22:23:40 +0800

    change

--- permiss/controller/System.php
+++ permiss/controller/System.php
@@ -20,6 +20,7 @@ class System extends User
 
     /**
      * @method get
+     * 
      */
     public function getModuleOperateApi($mid)
     {
@@ -27,5 +28,20 @@ class System extends User
         $operates = \array_column($operates, 'alias');
         $operates = \array_combine($operates, array_fill(0, \count($operates), true));
         return [200, $operates];
+    }
+
+    /**
+     * @method post
+     * 
+     * @rule log|post|参数错误 array
+     */
+    public function frontLogApi()
+    {
+        $log = $this->request->post('log');
+
+        /**
+         * 保存到日志中
+         */
+        return [200, 'ok'];
     }
 }
--- vue/src/libs/error.js
+++ vue/src/libs/error.js
@@ -2,28 +2,28 @@ console.log('error')
 import ajax from '../api/axios'
 
 const errorHandler = (error, vm) => {
-  console.error(vm)
-  console.error(error)
-  if (process.env.NODE_ENV === 'production') {
-    let log = {
-      data: vm._data,
-      route: vm.$route.fullPath,
-      msg: {
-        desc: error.message,
-        stack: error.stack
-      }
+    console.error(vm)
+    console.error(error)
+    if (process.env.NODE_ENV === 'production') {
+        let log = {
+            data: vm._data,
+            route: vm.$route.fullPath,
+            msg: {
+                desc: error.message,
+                stack: error.stack
+            }
+        }
+        ajax.post('system/frontLog', {
+            log
+        })
     }
-    ajax.post('admin_sysframe/frontLog', {
-      log
-    })
-  }
 }
 
 export default {
-  install: function (Vue) {
-    Vue.config.errorHandler = errorHandler;
-    Vue.prototype.$throw = function (error) {
-      return errorHandler(error, this);
-    };
-  }
+    install: function(Vue) {
+        Vue.config.errorHandler = errorHandler;
+        Vue.prototype.$throw = function(error) {
+            return errorHandler(error, this);
+        };
+    }
 }
