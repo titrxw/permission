d6eeb6ff3e6ee6d0194161e4f10ea3fdfa2d965d
git <1120309488@qq.com>
2019-04-10 09:34:07 +0800

    change

--- vue/src/libs/hm.js
+++ vue/src/libs/hm.js
@@ -1,66 +1,81 @@
-var triggers = ['login-collect'];
+var triggers = [];
 
-function getEnv()
-{
-  return {
-    t: document.title,
-    w: window.screen.width,
-    h: window.screen.height,
-    pd: window.screen.pixelDepth,
-    cd: window.screen.colorDepth,
-    v: window.navigator.appVersion,
-    dm: document.domain,
-    u:document.URL,
-    r: document.referrer,
-    l: window.navigator.language,
-    c: document.cookie
-  }
-}
-
-function report (data) {
-  let url = 'https://test/hm?';
-  for (let key in data) {
-    url += key + '=' + data[key] + '&'
-  }
-  
-  new Image().src = url;  
-}
 
 let hm = {
-  comeIn() {
-    let info = getEnv()
-    info['e'] = 'load'
-    report(info)
-  },
-  comeOut() {
-    let info = getEnv()
-    info['e'] = 'unload'
-    report(info)
-  },
-  flag (e) {
-    if (triggers.indexOf(e) >= 0) {
-      return true
+  getElementByXpath(path) {
+    let xresult = document.evaluate(path, document, null, XPathResult.ANY_TYPE, null);
+    let xnodes = null;
+    let xres;
+    while (xres = xresult.iterateNext()) {
+      xnodes = xres;
     }
 
-    triggers.push(e)
+    return xnodes
+  },
+  getEnv() {
+    return {
+      t: document.title,
+      w: window.screen.width,
+      h: window.screen.height,
+      pd: window.screen.pixelDepth,
+      cd: window.screen.colorDepth,
+      v: window.navigator.appVersion,
+      dm: document.domain,
+      ln: encodeURIComponent(document.URL),
+      r: document.referrer,
+      l: window.navigator.language,
+      c: document.cookie
+    }
+  },
+  report (data) {
+    let url = 'https://test/hm?';
+    for (let key in data) {
+      url += key + '=' + data[key] + '&'
+    }
+    
+    new Image().src = url;  
+  },
+  trigger(e = null) {
+    let info = this.getEnv()
+    info['type'] = e
+    this.report(info)
+  },
+  flag (e) {
+    if (typeof e == 'array') {
+      e.forEach(element => {
+        if (triggers.indexOf(element.hm) < 0) {
+          let target = this.getElementByXpath(element.xpath)
+          if (target) {
+            target.setAttribute('data-hm', element.hm)
+            triggers.push(element.hm)
+          }
+        }
+      });
+    } else {
+      if (triggers.indexOf(e.hm) < 0) {
+        let target = this.getElementByXpath(e.xpath)
+        if (target) {
+          target.setAttribute('data-hm', e.hm)
+          triggers.push(e.hm)
+        }
+      }
+    }
   }
 }
 
 document.addEventListener('click', (e) => {
   if (e.target.dataset.hm) {
     if (triggers.indexOf(e.target.dataset.hm) >= 0) {
-      let info = getEnv()
-      info['e'] = e.target.dataset.hm
-      report(info)
+      hm.trigger(e.target.dataset.hm)
     }
   }
 });
 
 window.onload = (e) => {
-  hm.comeIn()
+  hm.trigger('load')
 }
 window.onunload = (e) => {
-  hm.comeOut()
+  hm.trigger('unload')
 }
 
 
